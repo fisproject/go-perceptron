7362d4f61039c92d882ce2c274bb749654c51665
egawa <tatsuya@TatsuyaEgawa-no-MacBook-Air.local>
2014-08-23 17:37:31 +0900

    add test

--- perceptron_test.go
+++ perceptron_test.go
@@ -2,9 +2,26 @@ package perceptron
 
 import (
 	"./perceptron"
+	"fmt"
+	"io/ioutil"
 	"testing"
 )
 
-func TestLerning(t *testing.T) {
+func TestLearning(t *testing.T) {
+	training_set, err := ioutil.ReadFile("./json/input.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	/* Initialise the weights and the threshold */
+	threshold := 0.5
+	learning_rate := 0.1
+	weights := []float64{0.0, 0.0, 0.0}
+
+	actual := perceptron.Learning(threshold, learning_rate, weights, training_set)
+	var expected error = nil
+	if actual != expected {
+		t.Errorf("got %v,want %v", actual, expected)
+	}
 }
